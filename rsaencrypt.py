ab5de6edccb69d0a1305f2df18fc1622b99f7c24
theEndurance-del <57960301+theEndurance-del@users.noreply.github.com>
2021-01-14 20:43:57 +0530

    Add files via upload

---
+++ rsaencrypt.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python3
+
+import random
+
+def encrypt(plain):
+
+    prime1    = 0
+    prime2    = 0
+    repeator  = 1
+    primelist = [prime1, prime2]
+
+    while (repeator==1):
+
+        prime1 = random.randint(0, 2048)
+        prime2 = random.randint(0, 2048)
+        count1 = 0
+        count2 = 0
+
+        for i in range (1, prime1 + 1):
+
+            modfactor = prime1 % i
+
+            if modfactor == 0:
+
+                count1 = count1 + 1
+
+        for i in range (1, prime2 + 1):
+
+            modfactor = prime2 % i
+
+            if modfactor == 0:
+
+                count2 = count2 + 1
+
+        if count1 == 2 and count2 == 2:
+
+            primelist = [prime1, prime2]
+            print     ("prime numbers", end=" ")
+            print     (primelist)
+            repeator  = 0
+
+        else:
+
+            repeator = 1
+
+
+    phi_n    = prime1 * prime2
+    lambda_n = (prime1 - 1) * (prime2 - 1)
+
+    print ("lambda(n) " + str(lambda_n) + " phi(n) " + str(phi_n))
+
+    exponent  = 0
+    repeator1 = 1
+
+    while (repeator1 == 1):
+
+        count    = 0
+        exponent = random.randint(1, 256)
+
+        for factor in range (1, phi_n):
+
+            modfactor1 = phi_n % factor
+            modfactor2 = exponent % factor
+
+            if modfactor1 == 0 and modfactor2 == 0:
+
+                count = count + 1
+
+        if count == 1:
+
+            print     ("exponent value " + str(exponent))
+            repeator1 = 0
+
+        else:
+
+            repeator1 = 1
+
+    # d=(1+(phi_n))/e
+    # x is any positive integer
+    print(" ")
+    repeator2 = 1
+    while (repeator2 == 1):
+        x = random.randint(0, 50)
+        d = (1 + x * (lambda_n)) / exponent
+        print("\r D value", d)
+
+        if d.is_integer():
+            repeator2 = 0
+
+        else:
+
+            repeator2 = 1
+
+    print ("public key  :({}, {})".format(phi_n, exponent))
+    print ("private key :({}, {})".format(phi_n, d))
+
+    print (plain)
+    ASCII = [ ord(x) for x in plain ]
+    print (ASCII)
+
+    rsa_encrypt_ascii = [ ((ord(x) ** exponent) % phi_n) for x in plain ]
+    print             (rsa_encrypt_ascii)
+    
+    rsa_encrypt = ''.join(chr(i) for i in rsa_encrypt_ascii)
+    print       (rsa_encrypt)
+
+    rsa_decrypt   = [ ((ord(x) ** d) % phi_n) for x in rsa_encrypt]
+    rsa_decrypted = ''.join(chr(i) for i in rsa_decrypt)
+
+plain_text   = input("plain text to be encrypted >> ")
+string_value = str(plain_text)
+encrypt      (string_value)
---
+++ smallenc.java
@@ -0,0 +1,113 @@
+import java.util.*;
+import java.io.IOException;
+
+public class smallenc
+{
+    public static boolean isInteger(float num)
+    {
+        return (num % 1 == 0);
+    }
+    public static int primegen()
+    {
+        Random rand = new Random();
+        int prime = 0;
+        while (true)
+        {
+            int seedcount = 0;
+            int seed = rand.nextInt(50) + 1;
+            for (int i = 1;i <= seed; i++)
+            {
+                int modfactor = seed % i;
+                if (modfactor == 0)
+                {
+                    seedcount = seedcount + 1;
+                }
+            }
+            if (seedcount == 2)
+            {
+                prime = seed;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    public static boolean gcd(int n, int m)
+    {
+        int count = 0;
+        for (int i = 1; i <= m; i++)
+        {
+            int mod1 = m % i;
+            int mod2 = n % i;
+            if (mod1 == 0 && mod2 == 0)
+            {
+                count = count + 1;
+            }
+        }
+        if (count == 1)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    public static int privatekey(int primeprod, int exp, int lambda) 
+    {
+        Random random = new Random();
+        int var = 0;
+        //euler's theorem is
+        //d = (1 + x*lambda) / e
+        //e = exp
+        //lambda as stated
+        //d is smaller than primeprod
+        //d = var
+        while(true)
+        {
+            int seed = random.nextInt(200) + 1;
+            System.out.println("seed: "+seed);
+            var = (1 + (seed*lambda)) / exp;
+            if(isInteger(var))
+            {
+                break;
+            }
+        }
+        return var;
+    }
+    public static int encrypt(int plain)
+    {
+        Random rand = new Random();
+        int prime1 = primegen();
+        int prime2 = primegen();
+        System.out.println(prime1+", "+prime2);
+
+        int n = prime1 * prime2;
+        int lambda_n = (prime1 - 1) * (prime2 - 1);
+
+        System.out.println(n+", "+lambda_n);
+        int exponent;
+        while (true)
+        {
+            int e = rand.nextInt(lambda_n);
+            if (gcd(e, lambda_n))
+            {
+                exponent = e;
+                break;
+            }
+        }
+        System.out.println(exponent);
+
+        // d.e = i mod (lambd_n)
+
+        int decrypt = privatekey(n, exponent, lambda_n);
+        System.out.println(decrypt);
+        return 0;
+    }
+    public static void main(String[] args) 
+    {
+        int num = encrypt(0);
+        System.out.println(num);
+    }
+}
