59e7cb662f5ce48c086bf706aa39c6df351f3fc0
theEndurance-del <57960301+theEndurance-del@users.noreply.github.com>
2021-01-27 22:35:04 +0530

    Update smallenc.java

--- smallenc.java
+++ smallenc.java
@@ -1,13 +1,45 @@
 import java.util.*;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Base64;
 
 public class smallenc
 {
+    public static int power(int n, int p)
+    {
+        int pow = 1;
+        for (int i = 1; i <= p;i++)
+        {
+            pow *= n;
+        }
+        System.out.println("power: "+pow);
+        return pow;
+    }
+
+    public static int[] StringToASCII(String value)
+    {
+        int length = value.length();
+        int[] arr = new int[length];
+        for(int i = 0; i < length; i++)
+        {
+            char ch = value.charAt(i);
+            arr[i] = (int)ch;
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        return arr;
+    }
+    public static String arrToString(char[] chrarray)
+    {
+        String string = String.valueOf(chrarray);
+        return string;
+    }
+
     public static boolean isInteger(double num)
     {
         return (num % 1 == 0);
     }
+
     public static int primegen()
     {
         Random rand = new Random();
@@ -15,7 +47,7 @@ public class smallenc
         while (true)
         {
             int seedcount = 0;
-            int seed = rand.nextInt(1024) + 1;
+            int seed = rand.nextInt(128) + 1;
             for (int i = 1;i <= seed; i++)
             {
                 int modfactor = seed % i;
@@ -71,8 +103,12 @@ public class smallenc
         int finalvar = (int)var;
         return finalvar;
     }
-    public static int encrypt(int[] plain)
+
+    public static int encrypt(int[] plain, int length)
     {
+        Base64.Encoder encoder = Base64.getEncoder();
+        Base64.Decoder decoder = Base64.getDecoder();
+
         Random rand = new Random();
         int prime1 = primegen();
         int prime2 = primegen();
@@ -95,6 +131,50 @@ public class smallenc
         System.out.println(exponent);
         int decrypt = privatekey(n, exponent, lambda_n);
         System.out.println(decrypt);
+        System.out.println("Public Key:  "+exponent+", "+n);
+        System.out.println("Private Key: "+decrypt+", "+n);
+
+        char[] encrypted = new char[length];
+
+        for(int i = 0;i < length; i++)
+        {
+            int var = plain[i];
+            int ePower = 1;
+            for(int j = 1; j <= exponent; j++)
+            {
+                ePower = ePower * var;
+                System.out.println("power: "+j);
+            }
+            int finalenc = ePower % n;
+            encrypted[i] = (char)finalenc;
+        }
+
+        System.out.println();
+
+        System.out.println("encrypted: "+arrToString(encrypted));
+
+        String rSAencryptedString = arrToString(encrypted);
+        String b64String = encoder.encodeToString(rSAencryptedString.getBytes());
+        System.out.println("encrypted: "+b64String);
+
+        char[] decrypted = new char[length];
+        String b64dString = new String(decoder.decode(b64String));
+
+        System.out.println(b64dString);
+        
+        int[] cipher = StringToASCII(b64dString);
+
+        for(int i = 0; i < b64dString.length(); i++)
+        {
+            int dPower = power(cipher[i], decrypt);
+            int finaldec = dPower % n;
+            dPower = 0;
+            decrypted[i] = (char)finaldec;
+        }
+
+        String rSAdecryptSting = arrToString(decrypted);
+
+        System.out.println("decrypted: "+rSAdecryptSting);
         return 0;
     }
 
@@ -103,19 +183,11 @@ public class smallenc
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter text to be encrypted: ");
         String plainText = scanner.nextLine();
-        int[] ASCII = new int[plainText.length()];
+        int[] ASCII = StringToASCII(plainText);
         int length = plainText.length();
-        for(int i = 0; i < length; i++)
-        {
-            char ch = plainText.charAt(i);
-            ASCII[i] = (int)ch;
-        }
-        for(int i = 0 ; i < length; i++)
-        {
-            System.out.print(ASCII[i]+", ");
-        }
+        
         System.out.println();
-        encrypt(ASCII);
+        encrypt(ASCII, length);
         scanner.close();
     }
 }
